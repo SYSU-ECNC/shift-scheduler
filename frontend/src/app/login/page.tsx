81d6a393cb4cd08c57cf778b3e99fe9e588290c1
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-23 13:06:30 +0800

    feat: 完成登录页面的外观部分

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/login/login-form";
+
+const LoginPage = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ frontend/src/components/login/login-form.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  username: z.string().min(1, {
+    message: "请输入用户名",
+  }),
+  password: z.string().min(1, {
+    message: "请输入密码",
+  }),
+});
+
+const LoginForm = () => {
+  const { register, handleSubmit } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>登录</CardTitle>
+        <CardDescription>
+          在下方输入用户名（NetID）和密码进行登录
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid gap-4">
+            <div className="grid gap-1.5">
+              <Label htmlFor="username">用户名</Label>
+              <Input
+                {...register("username")}
+                type="text"
+                id="username"
+                placeholder="请输入 NetID"
+              />
+            </div>
+            <div className="grid gap-1.5">
+              <Label htmlFor="password">密码</Label>
+              <Input
+                {...register("password")}
+                type="password"
+                id="password"
+                placeholder="请输入密码"
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full" type="submit" form="login-form">
+          登录
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default LoginForm;
