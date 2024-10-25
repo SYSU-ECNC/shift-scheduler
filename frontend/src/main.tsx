086802bfc1b094409fea107485a14f9c9f483cd9
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-25 23:23:54 +0800

    feat: 将 routes 文件夹改成 pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginPage from "./routes/auth/login-page";
+import LoginPage from "./pages/auth/login/login-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
