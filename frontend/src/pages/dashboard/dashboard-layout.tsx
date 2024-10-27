87c02357a911d550749877ce61846e8a3c640457
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-27 23:57:47 +0800

    feat: 给 layout 加上了 breadcrumb

--- frontend/src/pages/dashboard/dashboard-layout.tsx
+++ frontend/src/pages/dashboard/dashboard-layout.tsx
@@ -1,4 +1,13 @@
 import AppSidebar from "@/components/custom/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 
@@ -7,7 +16,21 @@ export default function DashboardLayout() {
     <SidebarProvider>
       <AppSidebar />
       <main>
-        <SidebarTrigger />
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>ECNC 排班系统</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
         <Outlet />
       </main>
     </SidebarProvider>
