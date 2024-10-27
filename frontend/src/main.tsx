047eef52f0094d228e202c32698f8e0f435c2ed0
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-27 15:51:22 +0800

    feat: 安装 shadcn/ui sonner 组件

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/auth/login/login-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "./components/ui/sonner";
 
 const queryClient = new QueryClient();
 
@@ -23,5 +24,7 @@ createRoot(document.getElementById("root")!).render(
         <ReactQueryDevtools initialIsOpen={false} />
       )}
     </QueryClientProvider>
+    {/* Toaster 需要通过 toastOptions 强制它使用别的字体 */}
+    <Toaster toastOptions={{ className: "font-display" }} />
   </StrictMode>
 );
