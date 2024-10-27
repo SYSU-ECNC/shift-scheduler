5b9115c6b7322c4407ad279aaf0b13e90350e4e5
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-27 16:52:18 +0800

    style: 将后端返回的字段改为蛇形命名法，方便前端保持命名一致性

--- backend/internal/domain/users.go
+++ backend/internal/domain/users.go
@@ -10,7 +10,7 @@ type User struct {
 	ID           uuid.UUID `json:"id"`
 	Username     string    `json:"username"`
 	PasswordHash string    `json:"-"`
-	FullName     string    `json:"full_name"`
+	FullName     string    `json:"fullName"`
 	Role         string    `json:"role"`
-	CreatedAt    time.Time `json:"created_at"`
+	CreatedAt    time.Time `json:"createdAt"`
 }
