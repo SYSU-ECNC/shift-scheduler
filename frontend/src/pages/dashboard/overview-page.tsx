5811ca3577d3067daace74046114ceb06b38c15c
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-29 15:16:37 +0800

    feat: 在主页加上一些提示语

--- frontend/src/pages/dashboard/overview-page.tsx
+++ frontend/src/pages/dashboard/overview-page.tsx
@@ -1,3 +1,10 @@
 export default function OverviewPage() {
-  return <div>Overview</div>;
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <span className="font-semibold">主页</span>
+      <span className="text-sm">
+        如果你看到这里一篇空白，不用担心，因为我还没想好这里放什么:)
+      </span>
+    </div>
+  );
 }
