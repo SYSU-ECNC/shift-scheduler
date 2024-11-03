6ba49baeb46fcb6f2dd56de22f7c325fcb94a368
jonathanhu237 <jonathanhu237@gmail.com>
2024-11-03 13:03:19 +0800

    feat: 没有权限的用户在侧边栏看不到对应的导航了

--- frontend/src/components/app-sidebar-content.tsx
+++ frontend/src/components/app-sidebar-content.tsx
@@ -9,17 +9,21 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "./ui/sidebar";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getMe } from "@/lib/api";
 
 const navMain = [
   {
     title: "主页",
     url: "/dashboard",
     icon: Home,
+    roleRequired: ["普通助理", "资深助理", "黑心"],
   },
   {
     title: "用户管理",
     url: "/dashboard/users",
     icon: UserCog,
+    roleRequired: ["黑心"],
   },
 ];
 
@@ -33,6 +37,10 @@ const navSecondary = [
 
 export default function AppSidebarContent() {
   const location = useLocation();
+  const { data: me } = useSuspenseQuery({
+    queryKey: ["me"],
+    queryFn: getMe,
+  });
 
   return (
     <SidebarContent>
@@ -40,19 +48,23 @@ export default function AppSidebarContent() {
         <SidebarGroupLabel>应用</SidebarGroupLabel>
         <SidebarGroupContent>
           <SidebarMenu>
-            {navMain.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton
-                  asChild
-                  isActive={location.pathname === item.url}
-                >
-                  <Link to={item.url}>
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
+            {navMain.map((item) => {
+              return (
+                item.roleRequired.includes(me.role) && (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      isActive={location.pathname == item.url}
+                    >
+                      <Link to={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                )
+              );
+            })}
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarGroup>
